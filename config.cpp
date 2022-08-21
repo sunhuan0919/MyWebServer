92b0fb94397c1c32745e2099eb13c58c5ae37e4d
Sun <2060207050@qq.com>
2022-08-21 10:31:53 +0800

    config parse

---
+++ config.cpp
@@ -0,0 +1,84 @@
+#include "config.h"
+
+Config::Config(){
+    //端口号,默认9006
+    PORT = 9006;
+
+    //日志写入方式，默认同步
+    LOGWrite = 0;
+
+    //触发组合模式,默认listenfd LT + connfd LT
+    //0，表示使用LT + LT
+    //1，表示使用LT + ET
+    //2，表示使用ET + LT
+    //3，表示使用ET + ET
+    TRIGMode = 0;
+
+    //优雅关闭链接，默认不使用
+    OPT_LINGER = 0;
+
+    //数据库连接池数量,默认8
+    sql_num = 8;
+
+    //线程池内的线程数量,默认8
+    thread_num = 8;
+
+    //关闭日志,默认不关闭
+    close_log = 0;
+
+    //并发模型,默认是proactor
+    actor_model = 0;
+}
+
+void Config::parse_arg(int argc, char*argv[]){
+    int opt;
+    const char *str = "p:l:m:o:s:t:c:a:";
+    while ((opt = getopt(argc, argv, str)) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+        {
+            PORT = atoi(optarg);
+            break;
+        }
+        case 'l':
+        {
+            LOGWrite = atoi(optarg);
+            break;
+        }
+        case 'm':
+        {
+            TRIGMode = atoi(optarg);
+            break;
+        }
+        case 'o':
+        {
+            OPT_LINGER = atoi(optarg);
+            break;
+        }
+        case 's':
+        {
+            sql_num = atoi(optarg);
+            break;
+        }
+        case 't':
+        {
+            thread_num = atoi(optarg);
+            break;
+        }
+        case 'c':
+        {
+            close_log = atoi(optarg);
+            break;
+        }
+        case 'a':
+        {
+            actor_model = atoi(optarg);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
